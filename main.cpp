59f4e6985418271c837a58741d69aa46b0da59b8
Franek <michalakfranciszek2005@gmail.com>
2025-11-03 01:23:33 +0100

    some cleaning

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include "headers.h"
-#include "Tscreen.cpp"
+#include "src/headers.h"
+#include "src/Tscreen.cpp"
 
 class Tscreen;
 
 int main(){
-
     Tscreen screen;
     //screen.clear();
     screen.plot(32,32,0,0,1);
